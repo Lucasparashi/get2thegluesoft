34cb130d083be50c7457b5655e96669a014c7533
Lucasparashi <zxcv98072825@gmail.com>
2025-05-22 05:03:45 +0800

    Add helloWorld1 folder

---
+++ helloWorld1/helloWorld1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace helloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnHW = new System.Windows.Forms.Button();
+            this.labelHW = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btnHW, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelHW, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnHW
+            // 
+            this.btnHW.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnHW.AutoSize = true;
+            this.btnHW.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHW.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnHW.Location = new System.Drawing.Point(345, 109);
+            this.btnHW.Name = "btnHW";
+            this.btnHW.Size = new System.Drawing.Size(110, 26);
+            this.btnHW.TabIndex = 0;
+            this.btnHW.Text = "Say Hello World";
+            this.btnHW.UseVisualStyleBackColor = true;
+            this.btnHW.Click += new System.EventHandler(this.btnHW_Click);
+            // 
+            // labelHW
+            // 
+            this.labelHW.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelHW.AutoSize = true;
+            this.labelHW.Font = new System.Drawing.Font("微軟正黑體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelHW.Location = new System.Drawing.Point(355, 225);
+            this.labelHW.Name = "labelHW";
+            this.labelHW.Size = new System.Drawing.Size(89, 81);
+            this.labelHW.TabIndex = 1;
+            this.labelHW.Text = "\"\"";
+            this.labelHW.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnHW;
+        private System.Windows.Forms.Label labelHW;
+    }
+}
+
